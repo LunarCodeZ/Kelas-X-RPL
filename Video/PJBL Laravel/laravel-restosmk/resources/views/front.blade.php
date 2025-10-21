b75118d4f56a38bf973243960e7ffaaa2ea3e5fd
LunarCodeZ <erik.atatawiyu@gmail.com>
2025-10-21 09:41:23 +0700

    PJBL Laravel Episode 2

---
+++ Video/PJBL Laravel/laravel-restosmk/resources/views/front.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aplikasi Restoran SMK</title>
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div class="mt-2">
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <a href="#"><img style="width:50px; border-radius:100%;" src="{{ asset('gambar/foto-profil.png') }}" alt=""></a>
+                    <ul class="navbar-nav gap-5">
+                        <li class="nav-item">Cart</li>
+                        <li class="nav-item">Register</li>
+                        <li class="nav-item">Email</li>
+                        <li class="nav-item">Login</li>
+                        <li class="nav-item">Logout</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="row">
+            <div class="col-2">
+                @foreach ($kategoris as $kategori)
+                    <li class="list-group-item">{{ $kategori -> kategori }}</li>
+                @endforeach
+            </div>
+
+            <div class="col-10">
+                @yield('content');
+            </div>
+        </div>
+        <div>
+            Footer
+        </div>
+    </div>
+
+    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
