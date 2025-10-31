a741c575d6d42fb10bae240ba53202d2558de6b9
LunarCodeZ <erik.atatawiyu@gmail.com>
2025-11-01 06:09:20 +0700

    PJBL Laravel Episode 7

--- Video/PJBL Laravel/laravel-restosmk/app/Http/Controllers/KategoriController.php
+++ Video/PJBL Laravel/laravel-restosmk/app/Http/Controllers/KategoriController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Kategori;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreKategoriRequest;
 use App\Http\Requests\UpdateKategoriRequest;
 
@@ -15,8 +16,8 @@ class KategoriController extends Controller
      */
     public function index()
     {
-        //
-        return view('Backend.kategori.select');
+        $kategoris = Kategori::all();
+        return view('Backend.kategori.select',['kategoris'=>$kategoris]);
     }
 
     /**
@@ -27,6 +28,7 @@ class KategoriController extends Controller
     public function create()
     {
         //
+        return view('Backend.kategori.insert');
     }
 
     /**
@@ -35,9 +37,17 @@ class KategoriController extends Controller
      * @param  \App\Http\Requests\StoreKategoriRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreKategoriRequest $request)
+    public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'kategori'=>'required'
+        ]);
+
+        Kategori::create([
+            'kategori'=>$data['kategori']
+        ]);
+
+        return redirect('admin/kategori');
     }
 
     /**
@@ -46,9 +56,10 @@ class KategoriController extends Controller
      * @param  \App\Models\Kategori  $kategori
      * @return \Illuminate\Http\Response
      */
-    public function show(Kategori $kategori)
+    public function show( $idkategori)
     {
-        //
+        Kategori::where('idkategori','=',$idkategori)->delete();
+        return redirect('admin/kategori');
     }
 
     /**
@@ -57,9 +68,10 @@ class KategoriController extends Controller
      * @param  \App\Models\Kategori  $kategori
      * @return \Illuminate\Http\Response
      */
-    public function edit(Kategori $kategori)
+    public function edit( $idkategori)
     {
-        //
+        $kategori = Kategori::where('idkategori',$idkategori)->first();
+        return view('Backend.kategori.update',['kategori'=>$kategori]);
     }
 
     /**
@@ -69,9 +81,17 @@ class KategoriController extends Controller
      * @param  \App\Models\Kategori  $kategori
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateKategoriRequest $request, Kategori $kategori)
+    public function update(Request $request,  $idkategori)
     {
-        //
+        $data = $request->validate([
+            'kategori'=>'required'
+        ]);
+
+        Kategori::where('idkategori',$idkategori)->update([
+            'kategori'=>$data['kategori']
+        ]);
+
+        return redirect('admin/kategori');
     }
 
     /**
