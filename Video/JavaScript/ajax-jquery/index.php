64d7fb8fddbe6dc7b23cf0cd4368ba5a26abe694
LunarCodeZ <erik.atatawiyu@gmail.com>
2025-11-19 23:13:29 +0700

    JavaScript Episode 35

--- Video/JavaScript/ajax-jquery/index.php
+++ Video/JavaScript/ajax-jquery/index.php
@@ -5,11 +5,66 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AJAX-JQuery Bootstrap PHP</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="js/jquery.js"></script>
+    <script src="js/app.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Belajar Ajax JQuery Bootstrap PHP</h1>
+    
+    <div class="container">
+      <div class="row mt-4 text-center">
+        <h1>Belajar Ajax JQuery Bootstrap PHP</h1>
+      </div>
+      <div class="row mt-4">
+        <div class="col">
+          <div class="row">
+            <h2>Input Data Pelanggan</h2>
+          </div>
+          <div class="row">
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Pelanggan</label>
+                <input type="text" class="form-control" id="pelanggan" aria-describedby="emailHelp" required>
+                <div id="emailHelp" class="form-text">Harus di isi</div>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Alamat</label>
+                <input type="text" class="form-control" id="alamat">
+                <div id="emailHelp" class="form-text">Harus di isi</div>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Telp</label>
+                <input type="text" class="form-control" id="telp">
+                <div id="emailHelp" class="form-text">Harus di isi</div>
+              </div>
+              
+              <button type="submit" id="submit" class="btn btn-success">Submit</button>
+          </form>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="row">
+            <h2>Data Pelanggan</h2>
+          </div>
+          <div class="row">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">No</th>
+                  <th scope="col">Pelanggan</th>
+                  <th scope="col">Alamat</th>
+                  <th scope="col">Telp</th>
+                </tr>
+              </thead>
+              <tbody id="isidata">
+                
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
 
 
 
