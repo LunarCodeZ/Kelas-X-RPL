b9935ac6852c7fd4b65714ba14913574b04b1172
LunarCodeZ <erik.atatawiyu@gmail.com>
2026-02-24 12:20:31 +0700

    Java: 24 Feb 2026

--- Tugas Pak Tri/BelajarOOP/src/main/java/MuridRpl.java
+++ Tugas Pak Tri/BelajarOOP/src/main/java/MuridRpl.java
@@ -3,4 +3,19 @@ public class MuridRpl {
     String nama = "Erik Yanuar Putra";
     int absen = 10;
     int umur = 17;
+    
+    void dataSiswa() {
+        System.out.println("Nama saya adalah: " + nama);
+        System.out.println("Absen saya ialah: " + absen);
+        System.out.println("Umur saya ialah: " + umur);
+    }
+    
+    int luas;
+    int p = 20;
+    int l = 10;
+    
+    int menghitungPersegi() {
+        luas = p * l;
+        return luas;
+    }
 }
--- Tugas Pak Tri/BelajarOOP/src/main/java/SepuluhRpl.java
+++ Tugas Pak Tri/BelajarOOP/src/main/java/SepuluhRpl.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SepuluhRpl {
 
     public static void main(String[] args) {
@@ -5,10 +7,16 @@ public class SepuluhRpl {
         // Rumus: <namaClass> <objek> = new <namaClass>()
         // Objek MuridRpl
         System.out.println("=== Objek: MuridRpl ===");
-        MuridRpl siswa = new MuridRpl();
-        System.out.println("Nama: " + siswa.nama);
-        System.out.println("Absen: " + siswa.absen);
-        System.out.println("Umur: " + siswa.umur);
+        MuridRpl siswa1 = new MuridRpl();
+        System.out.println("Nama: " + siswa1.nama);
+        System.out.println("Absen: " + siswa1.absen);
+        System.out.println("Umur: " + siswa1.umur);
+        
+        System.out.println("\n=== Procedure ===");
+        siswa1.dataSiswa();
+        
+        System.out.println("\n=== Function ===");
+        System.out.println("Luas persegi: " + siswa1.menghitungPersegi());
         System.out.println("");
         
         // Objek KelompokTiga
@@ -17,7 +25,27 @@ public class SepuluhRpl {
         for (int i = 0; i < kelompok.anggota.length; i++) {
             System.out.println("Anggota " + (i + 1) + ": " + kelompok.anggota[i] + "/" + kelompok.absen[i]);
         }
+        System.out.println("");
         
+        // Dinamis
+//        MuridRpl siswa2 = new MuridRpl();
+//        Scanner input = new Scanner(System.in);
+//        
+//        System.out.print("Masukkan nama: ");
+//        String nama = input.nextLine();
+//        siswa2.nama = nama;
+//        System.out.print("Masukkan umur: ");
+//        int angka = input.nextInt();
+//        siswa2.umur = angka;
+//        System.out.print("Masukkan absen: ");
+//        angka = input.nextInt();
+//        siswa2.absen = angka;
+//        
+//        System.out.println("");
+//        System.out.println("=== Hasil ===");
+//        System.out.println("Nama: " + siswa2.nama);
+//        System.out.println("Umur: " + siswa2.umur);
+//        System.out.println("Absen: " + siswa2.absen);
         
     }
     
