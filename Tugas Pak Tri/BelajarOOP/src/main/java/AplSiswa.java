d57a8e52a882f6b7de5a68b8397de009da79ebb7
LunarCodeZ <erik.atatawiyu@gmail.com>
2026-04-20 10:22:09 +0700

    Java: 20 April 2026

---
+++ Tugas Pak Tri/BelajarOOP/src/main/java/AplSiswa.java
@@ -0,0 +1,10 @@
+public class AplSiswa {
+    
+    public static void main(String[] args) {
+        Siswa objek1 = new Siswa();
+        Siswa objek2 = new Siswa("Lunar");
+        Siswa objek3 = new Siswa("Lunar", 18);
+        Siswa objek4 = new Siswa("Lunar", 17, "Star Palace");
+    }
+    
+}
